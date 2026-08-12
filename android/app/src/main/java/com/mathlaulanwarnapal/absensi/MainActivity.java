1bae3887fedfac8da62b240c6e5156b7d48236c3
unknown <muhamadalif@gmail.com>
2026-08-12 14:45:43 +0700

    add android capacitor setup

---
+++ android/app/src/main/java/com/mathlaulanwarnapal/absensi/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mathlaulanwarnapal.absensi;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mathlaulanwarnapal.absensi',
+  appName: 'Absensi Guru MTS Mathlaul Anwar Napal',
+  webDir: 'dist'
+};
+
+export default config;
