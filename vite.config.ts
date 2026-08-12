8c357994a192ae97e6d4b8b5e4f63a5dc8721948
unknown <muhamadalif@gmail.com>
2026-08-12 14:54:57 +0700

    configure netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import path from "path";
 
 export default defineConfig({
@@ -9,15 +10,18 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "server" },
     }),
+    netlify(),
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     tsconfigPaths: true,
     alias: {
       "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
+
   server: {
     port: 3000,
     host: true,
